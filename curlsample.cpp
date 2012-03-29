2d3765d21eebbe17e87057f7a77289a37ef12c9d
Tagaki Aki <a-tagaki@esm.co.jp>
2012-03-29 12:30:41 +0900

    finished!!

--- curlsample.cpp
+++ curlsample.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<fstream>
 #include <string>
 #include "curl/curl.h" //load_path
 
@@ -6,7 +7,14 @@ using namespace std;
 
 static char errorBuffer[CURL_ERROR_SIZE];
 static string buffer;
-static int writer(){
+static int writer(char *data, size_t size, size_t nmemb,string *buffer){
+  int result = 0;
+
+  if (buffer != NULL) {
+    buffer->append(data, size * nmemb); //番地がメソッド的なものを使うときは -> を使う
+    result = size * nmemb;
+  }
+  return result;
 }
 
 int usage() {
@@ -27,13 +35,27 @@ int main(int argc, char **argv) {
 
     if (curl) {
       curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errorBuffer);
-      curl_easy_setopt(curl, CURLOPT_URL, &url);
+      curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
       curl_easy_setopt(curl, CURLOPT_HEADER, 0);
       curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
       curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writer);
       curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+
+      result = curl_easy_perform(curl);
+
+      curl_easy_cleanup(curl);
+
+      if (result == CURLE_OK){
+        //cout << buffer << "\n";
+        basic_ofstream<char> ofs(argv[1]);
+        ofs << buffer << endl; // ファイルに出力する
+      } else {
+        cout << "Error: [" << result << "] - " << errorBuffer << endl;
+        exit(-1);
+      }
     }
   } else {
     usage();
   }
+  return 0;
 }
